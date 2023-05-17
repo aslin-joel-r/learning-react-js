ec430b63bc6571ef3514de7a9db74e68c4daf5d1
aslin-r <aslinjoelr123@gmail.com>
2023-05-17 21:44:11 +0530

    used variables embedded in the html

--- client/src/MyInput.js
+++ client/src/MyInput.js
@@ -1,11 +1,16 @@
 import MyApp from './MyApp.css'
 
 export default function MyInput(){
+    const info={
+        name:'Aslin',
+        age:20,
+
+    }
     return(
         <div>
             <h3>Enter your name :</h3>
             <input type='text'></input>
-        
+            <h4>My name is {info.name} and I am {info.age} years old</h4>
         </div>
     )
 }
