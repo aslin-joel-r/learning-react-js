62370deb529745f960f1bffc38bdaff8549894c6
aslin-r <aslinjoelr123@gmail.com>
2023-05-17 20:57:43 +0530

    created external style sheet

--- client/src/MyInput.js
+++ client/src/MyInput.js
@@ -1,7 +1,11 @@
+import MyApp from './MyApp.css'
+
 export default function MyInput(){
     return(
         <div>
+            <h3>Enter your name :</h3>
             <input type='text'></input>
+        
         </div>
     )
 }
