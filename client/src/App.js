7b7271413141bf47568a22f3d48acc52ee11bf6c
aslin-r <aslinjoelr123@gmail.com>
2023-05-19 10:47:38 +0530

    tried objects

--- client/src/App.js
+++ client/src/App.js
@@ -5,14 +5,24 @@ import MyInput from './MyInput.js';
 function App() {
   const a=true;
   let content;
-    
+  
+  const products = [
+    { title: 'Cabbage', id: 1 },
+    { title: 'Garlic', id: 2 },
+    { title: 'Apple', id: 3 },
+  ];
+  const listItems = products.map(product =>
+    <li key={product.id}>
+      {product.title}
+    </li>
+  );
   return (
     
-    <div className="App">
-      {a?
-        <MyInput />:
-      <MyButton />
-    }
+   
+    <div>
+    
+      <ul>{listItems}</ul>
+    
     </div>
   );
 }
