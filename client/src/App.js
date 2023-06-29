2a52219aeb4457189f59fbdce58f1ac2059140cb
aslin-r <aslinjoelr123@gmail.com>
2023-06-29 12:11:34 +0530

    learning react from meta

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,10 @@
 import './App.css';
-import MyButton from './components/MyButton.js';
-import MyInput from './components/MyInput.js';
-import { useState } from 'react';
+import add from './components/add.js'
 
 function App() {
-  const [count,setCount]=useState(0);
-    function clickedMe(){
-        setCount(count+1);
-    }
-
-  return (
-    <div>
-    
-      <MyButton count={count} onClick={clickedMe} name={'button one'}/>
-      <MyButton count={count} onClick={clickedMe} name={'button two'}/>
-    </div>
-  );
+  add(39,5);
+  
+  return 1;
 }
 
 export default App;
