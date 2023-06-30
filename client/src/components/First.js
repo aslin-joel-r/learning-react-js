355f8898580d02f6d6db7a57ac0c8fa8036facc5
aslin-r <aslinjoelr123@gmail.com>
2023-06-30 21:05:40 +0530

    learned some new stuffs

--- client/src/components/First.js
+++ client/src/components/First.js
@@ -1,9 +1,10 @@
 function First(props){
 
-    const style={
-        color:'red',
-
-    }
+    const style = {
+        color: 'red',
+        fontSize: '200px',
+        backgroundColor: 'black'
+    };
     console.log(props)
     return <h2 style={style} >Hello ,{props.name} </h2>
 }
