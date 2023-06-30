78e2d5c591cc0b11b71ece26552922717e961a22
aslin-r <aslinjoelr123@gmail.com>
2023-06-30 12:15:53 +0530

    created a component with in the app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 
-
+function Component(){
+  return <h1>I am a Component</h1>
+}
 function App() {
 
   const msg='HEllo welcomes to the React  world'
-  return <h1>{msg}</h1>;
+  return <h1><Component /></h1>;
 }
 
 export default App;
