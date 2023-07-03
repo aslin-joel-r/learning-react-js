a75321a8bc3743ceb748c5ede7c3924497849b77
aslin-r <aslinjoelr123@gmail.com>
2023-07-03 19:53:02 +0530

    making it for next module

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,10 @@
 import './App.css';
-import Card from './components/Card.js'
-
 function App() {
 
   return (
     <div>
-    <h1>Task: Add three Card elements</h1>
-    <Card h2="First card's h2" h3="First card's h3"/>
-    <Card h2="Second card's h2" h3="Second card's h3"/>
-    <Card h2="Third card's h2" h3="Third card's h3"/>
+  
+  
     </div>
   )
  
