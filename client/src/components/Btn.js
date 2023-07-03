b6a1e3867fffa5d476a17df3ab4ae30f0ceac4fd
aslin-r <aslinjoelr123@gmail.com>
2023-07-03 21:58:29 +0530

    finished event handling

--- client/src/components/Btn.js
+++ client/src/components/Btn.js
@@ -1,12 +1,19 @@
 function Btn(){
-
+    var darkmode=true;
+    const dark=<h1>Dark mode is on</h1>
+    const light=<h1>Light mode is on</h1>
     function clicked(){
-        console.log('separate function')
+        darkmode=!darkmode
+        
+        darkmode==true ? console.log("darkmode is on") : console.log("darkmode is off");
     }
     return (
+       <>
+        {darkmode==true ? dark : light}
         <button onClick={clicked}>
             Click me
         </button>
+        </>
     )
 }
 
