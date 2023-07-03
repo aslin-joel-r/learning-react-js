f5ad59140846bea049eed19317fae7b18d65d805
aslin-r <aslinjoelr123@gmail.com>
2023-07-03 15:30:20 +0530

    completed the final lab in week 1

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
-import First from './components/First';
-import Second from './components/Second';
-import MyInput from './components/MyInput';
+import Card from './components/Card.js'
 
 function App() {
 
   return (
     <div>
-     <Second/>
+    <h1>Task: Add three Card elements</h1>
+    <Card h2="First card's h2" h3="First card's h3"/>
+    <Card h2="Second card's h2" h3="Second card's h3"/>
+    <Card h2="Third card's h2" h3="Third card's h3"/>
     </div>
   )
  
--- client/src/components/Second.js
+++ client/src/components/Second.js
@@ -4,7 +4,7 @@ const Second=()=> {
     val = val ==4 ? console.log('if statement'):console.log('else statement');
     return (
         <div>
-            <h2>{val ==4 ? 'if statement':'else statement'}</h2>
+            <h2>{ Math.round(8.4) }</h2>
         </div>
     )
 }
