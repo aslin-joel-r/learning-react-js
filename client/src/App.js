89699f20bcf685529cc19fd95d24bc0171cc306d
aslin-r <aslinjoelr123@gmail.com>
2023-07-05 20:37:59 +0530

    finished the lab in week-2

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,21 @@
-import Btn from './components/Btn.js'
-import {useState} from 'react'
+import Fruits from "./components/Fruits";
+import FruitsCounter from "./components/FruitsCounter";
+import React from "react";
 
 function App() {
 
-  const[count,setCount]=useState(0);
-
- function clicked(){
-    setCount(count+1)
-  }
+  const [fruits] = React.useState([
+    {fruitName: 'apple', id: 1},
+    {fruitName: 'apple', id: 2},
+    {fruitName: 'plum', id: 3},
+]);
   return (
-    <div>
-       
-    <h1>You have clicked {count} times</h1>
-    <button onClick={clicked}>Click me</button>
+    <div className="App">
+      <h1>Where should the state go?</h1>
+      <Fruits fruits={fruits}/>
+      <FruitsCounter count={fruits.length}/>
     </div>
-  )
- 
+  );
 }
 
 export default App;
---
+++ client/src/components/FruitsCounter.js
@@ -0,0 +1,7 @@
+function FruitsCounter(props) {
+    return (
+        <h2>Total fruits: {props.count}</h2>
+    )
+}
+
+export default FruitsCounter;
