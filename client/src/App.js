4616b13e0799b69262bd316aea4c4ddc1b542c6f
aslin-r <aslinjoelr123@gmail.com>
2023-07-05 10:54:08 +0530

    tried hooks

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,18 @@
 import Btn from './components/Btn.js'
-
+import {useState} from 'react'
 
 function App() {
 
+  const[count,setCount]=useState(0);
+
+ function clicked(){
+    setCount(count+1)
+  }
   return (
     <div>
-        <Btn></Btn>
-  
+       
+    <h1>You have clicked {count} times</h1>
+    <button onClick={clicked}>Click me</button>
     </div>
   )
  
