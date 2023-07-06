7c5da06d69b5fc71fdbefa87332ca8812ce71ea0
aslin-r <aslinjoelr123@gmail.com>
2023-07-06 12:03:48 +0530

    removed the index.css

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,6 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
 import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -13,4 +11,4 @@ root.render(
   </BrowserRouter>
 );
 
-reportWebVitals();
+
