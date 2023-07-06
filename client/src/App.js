451a1352bdd6221068b43c71c4fd851ba5d18d49
aslin-r <aslinjoelr123@gmail.com>
2023-07-06 18:28:49 +0530

    cleaned

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,9 @@
-import Weekends from './components/Weekend';
-import Workdays from './components/Workdays';
-
 function App() {
 
-  var day=new Date().getDay();
-  day = 1
-  const msg=day>6 ? <Weekends/> : <Workdays/>
   
-  switch(day){
-    
-  }
   return (
     <div>
-     {day==1 && <h1>Today is Monday</h1>}
+     
     </div>
   )
 }
