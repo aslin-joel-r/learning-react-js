29837484730ee502d320016a4a66697191b3a9a1
aslin-r <aslinjoelr123@gmail.com>
2023-07-06 12:27:39 +0530

    finished the ungraded assignment-1 on week-3

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,21 @@
 import About from './components/About';
 import Home from './components/Home';
 import {Routes, Route,Link} from 'react-router-dom';
+import Contact from './components/Contact';
 
 function App() {
 
   return (
     <div>
       <nav>
-        <Link to='/'>Home</Link>
-        <Link to='/about'>About</Link>
+        <Link className='nav' to='/'>Home</Link>
+        <Link className='nav' to='/about'>About</Link>
+        <Link className='nav' to='/contact'>Contact</Link>
       </nav>
      <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/about' element={<About />} />
+      <Route path='/contact' element={<Contact />}/>
      </Routes>
     </div>
   )
