982faa9a65a1c8d4a9ec0d2c56b257485ef8587c
aslin-r <aslinjoelr123@gmail.com>
2023-07-06 10:51:37 +0530

    ready for the next module

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,8 @@
-import Fruits from "./components/Fruits";
-import FruitsCounter from "./components/FruitsCounter";
-import React from "react";
+
 
 function App() {
 
-  const [fruits] = React.useState([
-    {fruitName: 'apple', id: 1},
-    {fruitName: 'apple', id: 2},
-    {fruitName: 'plum', id: 3},
-]);
-  return (
-    <div className="App">
-      <h1>Where should the state go?</h1>
-      <Fruits fruits={fruits}/>
-      <FruitsCounter count={fruits.length}/>
-    </div>
-  );
+ 
 }
 
 export default App;
