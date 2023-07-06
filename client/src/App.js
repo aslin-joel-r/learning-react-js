60a0e09acab31a024f36f6d770da53fd3c330da4
aslin-r <aslinjoelr123@gmail.com>
2023-07-06 12:01:01 +0530

    understanded the diff btw link and a tags

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,14 @@
 import About from './components/About';
 import Home from './components/Home';
-import {Routes, Route} from 'react-router-dom';
+import {Routes, Route,Link} from 'react-router-dom';
 
 function App() {
 
   return (
     <div>
       <nav>
-        <a href='#'>Home</a>
-        <a href='#'>About</a>
+        <Link to='/'>Home</Link>
+        <Link to='/about'>About</Link>
       </nav>
      <Routes>
       <Route path='/' element={<Home />} />
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,7 +1,7 @@
 function Home(){
     return (
         <h1>
-            I am a home page
+            Welcome
         </h1>
     )
 }
