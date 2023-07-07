9e8dabc1afab18735d6610fe33ca0afdf12df907
aslin-r <aslinjoelr123@gmail.com>
2023-07-07 19:26:36 +0530

    coursera final project completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,61 @@
-import React from "react";
+import {
+  useState,
+  useRef
+} from "react";
+import "./App.css";
 
 function App() {
+  const inputRef = useRef(null);
+  const resultRef = useRef(null);
+  const [result, setResult] = useState(0);
 
-  const bird1 = new Audio(
-    "https://upload.wikimedia.org/wikipedia/commons/9/9b/Hydroprogne_caspia_-_Caspian_Tern_XC432679.mp3"
-  );
-
-  const bird2 = new Audio(
-    "https://upload.wikimedia.org/wikipedia/commons/b/b5/Hydroprogne_caspia_-_Caspian_Tern_XC432881.mp3"
-  );
-
-  function toggle1() {
-    if (bird1.paused) {
-      bird1.play();
-    } else {
-      bird1.pause();
-    }
+  function plus(e) {
+    e.preventDefault();
+    setResult((result) => result + Number(inputRef.current.value));
   };
 
-  function toggle2() {
-    if (bird2.paused) {
-      bird1.play();
-    } else {
-      bird2.pause();
-    }
+  function minus(e) {
+    setResult((result) => result - Number(inputRef.current.value));
+  };
+
+  function times(e) {
+    setResult((result) => result * Number(inputRef.current.value));
+  };
+
+  function divide(e) {
+    setResult((result) => result / Number(inputRef.current.value));
+  };
+
+  function resetInput(e) {
+    inputRef.current.value = "";
+  };
+
+  function resetResult(e) {
+    setResult(0);
   };
 
   return (
-    <div>
-      <button onClick={toggle1}>Caspian Tern 1</button>
-      <button onClick={toggle2}>Caspian Tern 2</button>
+    <div className="App">
+      <div>
+        <h1>Simplest Working Calculator</h1>
+      </div>
+      <form>
+        <p ref={resultRef}>
+          {result}
+        </p>
+        <input
+          pattern="[0-9]"
+          ref={inputRef}
+          type="number"
+          placeholder="Type a number"
+        />
+        <button onClick={plus}>add</button>
+        <button onClick={minus}>subtract</button>
+        <button onClick={times}>multiply</button>
+        <button onClick={divide}>divide</button>
+        <button onClick={resetInput}>reset input</button>
+        <button onClick={resetResult}>reset result</button>
+      </form>
     </div>
   );
 }
